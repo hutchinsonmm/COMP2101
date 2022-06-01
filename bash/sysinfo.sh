b01f3d3c819b5c9bcb804ab0d392c53aa707a476
hutchinsonmm <200421484@student.georgianc.on.ca>
2022-06-01 13:44:24 -0400

    Fixed spacing of command and updated comments

--- bash/sysinfo.sh
+++ bash/sysinfo.sh
@@ -2,8 +2,8 @@
 # Bash Lab 1
 # Output of basic computer infomation
 
-echo "Host Name: $(hostname)" #Displays Hostname
-echo "Domain Name: $(domainname)" #Displays Domain name
-echo "OS Name and Version: $(egrep '^(VERSION|PRETTY_NAME)=' /etc/os-release)" #Gives only the version and OS name from the OS-release file
-echo "IP Addresses: $(ip a | grep -w inet | grep -v 127) "
-echo "Root Filesystem Status: $(df /root)" #Gives the used and avalable storage of the Root file system
+echo "Host Name: $(hostname)" #Displays Hostname.
+echo "Domain Name: $(domainname)" #Displays Domain name.
+echo "OS Name and Version: $(egrep '^(VERSION|PRETTY_NAME)=' /etc/os-release)" #Gives only the version and OS name from the OS-release file.
+echo "IP Addresses: $(ip a | grep -w inet | grep -v 127)" # Display's the ipv4 address for my network exculding any on the 127 network.
+echo "Root Filesystem Status: $(df /root)" #Gives the used and avalable storage of the Root file system.
